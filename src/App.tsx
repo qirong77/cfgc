0f330fe0e5ff995d22ccc9bbd3827391f026a617
qirong77 <qirong7777@gmail.com>
2025-11-25 14:53:37 +0800

    feat: 功能开发 ✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export default function App() {
     return (
-        <div className="app-container">
+        <div className="app-container" style={{ position: "relative" }}>
             <img src="/src/reference/9a.png" alt="content" />
             <div
                 className="title"
@@ -9,23 +9,89 @@ export default function App() {
                     position: "absolute",
                     top: "37vw",
                     left: 0,
-                    width: "100%",
+                    width: "100vw",
                     height: "5vw",
                     backgroundColor: "black",
                     opacity: 0.5,
                     display: "flex",
-                    justifyContent: 'space-around',
-                    padding:'0 8vw',
+                    justifyContent: "space-around",
+                    padding: "0 8vw",
                     alignItems: "center",
                 }}
             >
-              <button>1</button>
-              <button>2</button>
-              <button>3</button>
-              <button>4</button>
-
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
             </div>
-            <div></div>
+            <div
+                className="title1"
+                style={{
+                    background: "color",
+                    position: "absolute",
+                    top: "72vw",
+                    left: 0,
+                    width: "100vw",
+                    height: "5vw",
+                    backgroundColor: "black",
+                    opacity: 0.5,
+                    display: "flex",
+                    justifyContent: "space-around",
+                    padding: "0 8vw",
+                    alignItems: "center",
+                }}
+            ></div>
+            <div
+                className="title2"
+                style={{
+                    background: "color",
+                    position: "absolute",
+                    top: "120vw",
+                    left: 0,
+                    width: "100vw",
+                    height: "5vw",
+                    backgroundColor: "black",
+                    opacity: 0.5,
+                    display: "flex",
+                    justifyContent: "space-around",
+                    padding: "0 8vw",
+                    alignItems: "center",
+                }}
+            ></div>
+            <div
+                className="title3"
+                style={{
+                    background: "color",
+                    position: "absolute",
+                    top: "155vw",
+                    left: 0,
+                    width: "100vw",
+                    height: "5vw",
+                    backgroundColor: "black",
+                    opacity: 0.5,
+                    display: "flex",
+                    justifyContent: "space-around",
+                    padding: "0 8vw",
+                    alignItems: "center",
+                }}
+            ></div>
+            <div
+                className="title4"
+                style={{
+                    background: "color",
+                    position: "absolute",
+                    top: "192vw",
+                    left: 0,
+                    width: "100vw",
+                    height: "5vw",
+                    backgroundColor: "black",
+                    opacity: 0.5,
+                    display: "flex",
+                    justifyContent: "space-around",
+                    padding: "0 8vw",
+                    alignItems: "center",
+                }}
+            ></div>
         </div>
     );
 }
