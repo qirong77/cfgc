10f2d6ec817a420a490c1769d5404f074b3cd26d
qirong77 <qirong7777@gmail.com>
2025-11-25 14:38:53 +0800

    feat: 功能开发 ✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 export default function App() {
-  return (
-    <div className="app-container">
-      <img src="/src/reference/9a.png" alt="content" />
-    </div>
-  );
-} 
+    return (
+        <div className="app-container">
+            <img src="/src/reference/9a.png" alt="content" />
+            <div
+                className="title"
+                style={{
+                    background: "color",
+                    position: "absolute",
+                    top: "37vw",
+                    left: 0,
+                    width: "100%",
+                    height: "5vw",
+                    backgroundColor: "black",
+                    opacity: 0.5,
+                    display: "flex",
+                    justifyContent: 'space-around',
+                    padding:'0 8vw',
+                    alignItems: "center",
+                }}
+            >
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>4</button>
+
+            </div>
+            <div></div>
+        </div>
+    );
+}
