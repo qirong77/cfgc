7a77d3124eebf8c8a0503199464bea0df6dd9080
qirong77 <qirong7777@gmail.com>
2025-11-25 15:40:56 +0800

    feat: 功能开发 ✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,55 @@
+
 import React from "react";
 
+const commonStyle: React.CSSProperties = {
+    position: "absolute",
+    left: 0,
+    width: "100vw",
+    height: "8vw",
+    backgroundColor: "black",
+    opacity: 0.5,
+    display: "flex",
+    justifyContent: "space-around",
+    padding: "0 8vw",
+    alignItems: "center",
+};
+
+const titles = [
+    { className: "title1", top: "70vw" },
+    { className: "title2", top: "120vw" },
+    { className: "title3", top: "153vw" },
+    { className: "title4", top: "190vw" },
+];
+
+function scrollToTitle(className: string) {
+    const el = document.querySelector(`.${className}`);
+    el?.scrollIntoView({ behavior: "smooth" });
+}
+
 export default function App() {
-    const commonStyle: React.CSSProperties = {
-        background: "color",
-        position: "absolute",
-        top: "37vw",
-        left: 0,
-        width: "100vw",
-        height: "8vw",
-        backgroundColor: "black",
-        opacity: 0.5,
-        display: "flex",
-        justifyContent: "space-around",
-        padding: "0 8vw",
-        alignItems: "center",
-    };
     return (
         <div className="app-container" style={{ position: "relative" }}>
             <img src="/src/reference/9a.png" alt="content" />
             <div
                 className="nav-buttons"
                 style={{
-                    background: "color",
                     position: "absolute",
                     top: "37vw",
                 }}
             >
-                <button
-                    onClick={() => {
-                        document.querySelector(".title1")?.scrollIntoView();
-                    }}
-                >
-                    1
-                </button>
-                <button
-                    onClick={() => {
-                        document.querySelector(".title2")?.scrollIntoView();
-                    }}
-                >
-                    2
-                </button>
-                <button
-                    onClick={() => {
-                        document.querySelector(".title3")?.scrollIntoView();
-                    }}
-                >
-                    3
-                </button>
-                <button
-                    onClick={() => {
-                        document.querySelector(".title4")?.scrollIntoView();
-                    }}
-                >
-                    4
-                </button>
+                {titles.map((item, idx) => (
+                    <button key={item.className} onClick={() => scrollToTitle(item.className)}>
+                        {idx + 1}
+                    </button>
+                ))}
             </div>
-            <div
-                className="title1"
-                style={{
-                    ...commonStyle,
-                    top: "70vw",
-                }}
-            ></div>
-            <div
-                className="title2"
-                style={{
-                    ...commonStyle,
-                    top: "120vw",
-                }}
-            ></div>
-            <div
-                className="title3"
-                style={{
-                    ...commonStyle,
-                    top: "153vw",
-                }}
-            ></div>
-            <div
-                className="title4"
-                style={{
-                    ...commonStyle,
-                    top: "190vw",
-                }}
-            ></div>
+            {titles.map((item) => (
+                <div
+                    key={item.className}
+                    className={item.className}
+                    style={{ ...commonStyle, top: item.top }}
+                ></div>
+            ))}
         </div>
     );
 }
